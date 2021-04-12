a914856c4b44281e731d5f543799ce4d7068e24a
Jake Cooper <jake.elijah.cooper@gmail.com>
2021-04-12 02:17:38 -0700

    Comment

--- internal/generator/client.go
+++ internal/generator/client.go
@@ -1,5 +1,8 @@
 package generator
 
+import "fmt"
+
+// TODO generate the whole fkn thing with machineboxgql
 func GenerateClient(name string) string {
-	return "func (c *Client) %s (ctx context.Context, req %s) *%s {}"
+	return fmt.Sprintf("func (c *Client) %s (ctx context.Context, req %sRequest) *%s {}", name, name, name)
 }
